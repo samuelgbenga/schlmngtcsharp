4778c74307aa5af64917c5797aa44cfe421c5474
samuelgbenga <samuelgbenga972@gmail.com>
2024-11-02 15:05:45 +0100

    its work now

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ builder.Services
     .AddAuth0WebAppAuthentication(options => {
         options.Domain = builder.Configuration["Auth0:Domain"];
         options.ClientId = builder.Configuration["Auth0:ClientId"]; 
+        options.CallbackPath = new PathString("/callback");
     });
 
 builder.Services.AddControllersWithViews();
